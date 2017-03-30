bdd379f15b4e06361ec8511fd00ace60f6e2c22e
Pierre Mancini <pierremncini@gmail.com>
2017-03-30 11:12:25 +0200

    Comparaison des nouveaux barcode CNG avec les barcodes imcomplet du RedCap.
    Permet de voir lever des warning et lister les records RedCap à mettre à jour.

--- cron_cng.py
+++ cron_cng.py
@@ -9,7 +9,11 @@ import bs4 as BeautifulSoup
 import requests
 import yaml
 import re
+# import pprint
 from redcap import Project
+
+
+# pp = pprint.PrettyPrinter(indent=1)
 
 
 def get_barcode(fastq):
@@ -47,15 +51,6 @@ def get_instrument(patient_id):
     return instrument_type
 
 
-def get_filename(set_url):
-    """ Return all filenames in the set ."""
-
-    page = requests.get(set_url, auth=(config['login'], config['password']))
-    soup = BeautifulSoup.BeautifulSoup(page.content, 'lxml')
-
-    return [filename.string for filename in soup.find_all('a') if re.search(r'fastq\.gz$', filename.string)]
-
-
 # TODO: Faire un objet set_parser
 
 # Lecture du fichier de configuration
@@ -81,23 +76,57 @@ project = Project(api_url, config['api_key'])
 
 # Regarde quels set sont déjà sur redcap
 fields_of_interest = ['path_on_cng', 'path_on_cng_rna', 'path_on_cng_constit']
-response = project.export_records(fields=fields_of_interest)
-set_redcap = [dict[field].split('/')[-1] for dict in response for field in dict
+filled_path = project.export_records(fields=fields_of_interest)
+set_redcap_complete = [dict[field].split('/')[-1] for dict in filled_path for field in dict
     if field in fields_of_interest and dict[field] is not '']
 
-# On fait le différence entre les set du cng et les set de redcap
-set_import = set([l[:-1] for l in href_set]) - set(set_redcap)
+# On fait la différence entre les set du cng et les set de redcap
+# On obtient les set qui n'ont pas encore reçus les info du CNG
+set_incomplete = set([l[:-1] for l in href_set]) - set(set_redcap_complete)
 
 
+def get_filename(set_url):
+    """ Return all filenames in the set."""
 
-# TODO:
-# Partie interogation CRF: les données du CRF seront stockées sur RedCap
-# dans les instruments du projet multipli
+    page = requests.get(set_url, auth=(config['login'], config['password']))
+    soup = BeautifulSoup.BeautifulSoup(page.content, 'lxml')
+
+    return [filename.string for filename in soup.find_all('a') if re.search(r'fastq\.gz$', filename.string)]
+
+
+# Pour tout les set incomplete il nous faut les barecodes (à extraire des filenames)
+nested_list_filename = [get_filename(config['url_cng'] + set) for set in set_incomplete]
+barcodes_cng = [get_barcode(filename) for sublist in nested_list_filename for filename in sublist]
+
+# Les indices d'un record correspondant au barcode
+barcode_index = ['germline_dna_cng_barcode', 'tumor_dna_barcode', 'rna_gcn_barcode']
+
+
+response_barcode = project.export_records(fields=barcode_index)
+# Boucle alternative 1
+# for cng_barcode in list_barcode:
+
+# Création du dict par barcode
+record_by_barcode = {record[index]: record
+              for record in response_barcode
+              for index in record
+              if index in barcode_index and record[index]}
+
+redcap_barcodes = [barcode for barcode in record_by_barcode]
+
+# redcap & CNG = record à completé dans redcap
+# CNG - redcap = les barcodes/record qui posent problème
+if set(barcodes_cng) - set(redcap_barcodes):
+    for barcode in set(barcodes_cng) - set(redcap_barcodes):
+        # TODO: log à la place d'un print pour la production
+        print('Warning: le barcode ' + barcode + ' est présent dans le CNG et pas dans le RedCap.')
+
+
 
 def set_to_records(set):
     """ Return a list of record dictionary from all files in a set."""
 
-    filenames = get_filename(config['url_cng'] + set)
+    filenames = get_filename(config['url_cng'] + set, soupObject)
 
     records = {}
 
@@ -109,7 +138,6 @@ def set_to_records(set):
         instrument_type = get_instrument(patient_id)
 
         records.setdefault(patient_id, [])
-
 
         # Ces info seront déjà présentent sur le RedCap après intérogation du CRF
         record = {'patient_id': patient_id, 'redcap_repeat_instrument': instrument_type,
@@ -150,9 +178,10 @@ def set_to_records(set):
     return records
 
 
-records = [record for record_dict in map(set_to_records, set_import) for record in record_dict]
+# records = [record for record_dict in map(set_to_records, set_import) for record in record_dict]
+# # -> ['B00HSEMDEFAULT', 'B00HSENDEFAULT', 'B00HSEYDEFAULT', 
 
+# print(records)
 
-print(records)
 # Partie importation des set
-project.import_records(records)
+# project.import_records(records)
