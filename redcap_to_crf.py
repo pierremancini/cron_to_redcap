e32bbb7c67f87fadd946df7b174468c6322dc4af
Pierre Mancini <pierremncini@gmail.com>
2017-11-03 17:02:19 +0100

    Génération du fichier à transférer depuis la réponse redcap.

--- redcap_to_crf.py
+++ redcap_to_crf.py
@@ -16,6 +16,8 @@ import argparse
 import sys
 import hashlib
 
+from pprint import pprint
+
 
 def get_ftp_md5(ftp, remote_path):
     m = hashlib.md5()
@@ -29,6 +31,59 @@ def md5(fname):
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
+
+
+def upload_file(local_path, remote_path, connection, timeout=6, max_tries=1):
+
+    """ Upload file on ftp server.
+
+        :param remote_path: Head + tail file path
+        :param server: Dictonnary {host: '', login: '', password: ''}
+    """
+
+    local_head, local_fname = os.path.split(local_path)
+    remote_head, remote_fname = os.path.split(remote_path)
+
+    for count in range(max_tries):
+        try:
+            try:
+                with ftplib.FTP_TLS(connection['host'], timeout=timeout) as ftps:
+
+                    ftps.set_debuglevel(2)
+
+                    ftps.login(connection['login'], connection['password'])
+                    # Encrypt all data, not only login/password
+                    ftps.prot_p()
+                    # Déclare l'IP comme étant de la famille v6 pour être compatible avec ftplib (même si on reste en v4)
+                    # cf: stackoverflow.com/questions/35581425/python-ftps-hangs-on-directory-list-in-passive-mode
+                    ftps.af = socket.AF_INET6
+                    ftps.cwd(remote_head)
+                    with open(os.path.join(local_path), 'rb') as file:
+                        ftps.storbinary('STOR {}'.format(local_fname), file)
+
+            # Si on a un timeout ça se passe comme prévu.
+            except socket.timeout as e:
+                print(e)
+
+                # On vérifié l'intégrité du fichier transféré
+                with ftplib.FTP_TLS(config['crf_host']) as ftps:
+                    ftps.login(connection['login'], connection['password'])
+                    ftps.prot_p()
+                    ftps.af = socket.AF_INET6
+                    ftp_md5 = get_ftp_md5(ftps, remote_path)
+
+                if ftp_md5 == md5(local_fname):
+                    print('md5 ok')
+                    return True
+                else:
+                    print('Wrong md5.')
+                    print('FTP upload: Attemp n°{} , failed to upload {}'.format(count + 1, local_fname))
+
+        except ftplib.all_errors as e:
+            print(e)
+            print('FTP upload: Attemp n°{} , failed to upload {}'.format(count + 1, local_fname))
+
+    return False
 
 
 def args():
@@ -48,37 +103,57 @@ with open(args.secret, 'r') as ymlfile:
     secret_config = yaml.load(ymlfile)
 config.update(secret_config)
 
+# Génération du fichier d'exportation vers CRF
+project = Project(config['redcap_api_url'], config['api_key'])
+response = project.export_records(forms='bioinformatic_analysis')
 
-path_crf_file = config['path_bioanalysis']
-head_crf, tail_crf = os.path.split(path_crf_file)
+# Strucure:
+# {field_label: {instrument: field_name}}
+redcap_fields = {}
 
-try:
-    with ftplib.FTP_TLS(config['crf_host'], timeout=1) as ftps:
-    
-        ftps.set_debuglevel(2)
-    
-        ftps.login(config['login_crf'], config['password_crf'])
-        # Encrypt all data, not only login/password
-        ftps.prot_p()
-        # Déclare l'IP comme étant de la famille v6 pour être compatible avec ftplib (même si on reste en v4)
-        # cf: stackoverflow.com/questions/35581425/python-ftps-hangs-on-directory-list-in-passive-mode
-        ftps.af = socket.AF_INET6
-        ftps.cwd(head_crf)
-    
-        ftps.retrlines('LIST')
-    
-        with open('data/test.txt', 'rb') as file:
-            ftps.storbinary('STOR {}'.format('test.txt'), file)
+bio_analysis = {}
 
-except socket.timeout:
-    with ftplib.FTP_TLS(config['crf_host']) as ftps:
-        ftps.login(config['login_crf'], config['password_crf'])
-        # Encrypt all data, not only login/password
-        ftps.prot_p()
-        # Déclare l'IP comme étant de la famille v6 pour être compatible avec ftplib (même si on reste en v4)
-        # cf: stackoverflow.com/que
-        ftps.af = socket.AF_INET6
-        ftp_md5 = get_ftp_md5(ftps, os.path.join(head_crf, 'test.txt'))
-        print(ftp_md5)
+# Utilise les metadata pour retrouver les champs correspondant au formulaire bioinformatic_analysis
+# ICI à changer
+for metadict in project.metadata:
+    if metadict['form_name'] == 'bioinformatic_analysis':
+        bio_analysis.setdefault(metadict['field_label'], metadict['field_name'])
 
-print(md5('data/test.txt'))
+    redcap_fields.setdefault(metadict['field_label'], {}).setdefault(metadict['form_name'], metadict['field_name'])
+
+
+# On veux les champs bioinformatic_analysis pour créer le header du fichier d'export
+# Puis on remplis avec les valeurs elle même
+header = ['Patient ID',
+    'Date if receipt of all needed files',
+    'Quality control',
+    'Availability in genVarXplorer for interpretation',
+    'If yes data of availability'] # ! Utiliser 'If yes, data of availability' avec ',' pour le label
+
+
+# Records utilisés pour tester redcap
+to_exclude = ['DEV1', 'DEV2', 'DEV3', 'SARC2', 'SARC3']
+
+extraction_filename = 'bioanalysis_import.txt'
+local_path = os.path.join('data', 'crf_extraction', extraction_filename)
+path_crf_file = os.path.join('MULTIPLI', extraction_filename)
+
+# Traitement de la réponse redcap
+
+# On enregistre le fichier en local aussi pour avoir un backup
+with open(local_path, 'w') as tsvfile:
+    csvwriter = csv.writer(tsvfile, delimiter='\t')
+    csvwriter.writerow(header)
+    for record in response[1:]:
+        if not record['redcap_repeat_instrument'] and not record['redcap_repeat_instance']:
+            if record['patient_id'] not in to_exclude:
+                row = [record['patient_id'],
+                record['date_receipt_files'],
+                record['quality_control'],
+                record['availab_genvarxplorer'],
+                record['data_of_availability']]
+                csvwriter.writerow(row)
+
+connection = {'host': config['crf_host'], 'login': config['login_crf'], 'password': config['password_crf']}
+# Attention, le fichier précédent sera écrasé
+upload_file(local_path, path_crf_file, connection)
